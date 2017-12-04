0f362a64c551672db9c823ef9b0ee0ea56af2b44
threem0126 <89479759@qq.com>
2017-12-04 11:10:07 +0800

    增加内置错误码

--- src/lib/class2api/GKErrors_Inner.js
+++ src/lib/class2api/GKErrors_Inner.js
@@ -5,6 +5,7 @@ import {GKErrorWrap} from './GKErrorWrap'
  * @type {{_TOKEN_LOGIN_INVALID, _NOT_ACCESS_PERMISSION, _NOT_SERVICE, _PARAMS_VALUE_EXPECT, _NO_RESULT, _SERVER_ERROR, _NOT_PARAMS}}
  */
 export const GKErrors = {
+    _TOKEN_PARSE_FAIL: GKErrorWrap(-9, `token解析失败`),
     _RULE_VALIDATE_ERROR: GKErrorWrap(-8, `权限认证过程中发生异常`),
     _TOKEN_LOGIN_INVALID: GKErrorWrap(-7, `请先登录`),
     _NOT_ACCESS_PERMISSION: GKErrorWrap(-6, `无访问权限`),
@@ -12,5 +13,5 @@ export const GKErrors = {
     _PARAMS_VALUE_EXPECT: GKErrorWrap(-4, `参数不符合预期`),
     _NO_RESULT: GKErrorWrap(-3, `无匹配结果`),
     _SERVER_ERROR: GKErrorWrap(-2, `服务发生异常`),
-    _NOT_PARAMS: GKErrorWrap(-1, `缺少参数`),
+    _NOT_PARAMS: GKErrorWrap(-1, `缺少参数`)
 }
