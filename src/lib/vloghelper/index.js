709c7b714b28425aa2ee955e8c089e909d7bf5ef
threem0126 <threem0126@gmail.com>
2019-03-14 14:58:05 +0800

    增加泛日志收集函数

--- src/lib/vloghelper/index.js
+++ src/lib/vloghelper/index.js
@@ -21,39 +21,59 @@ const vlog_setting = ({apiUrl,debugTrace=false})=> {
     start();
 }
 
-const vlogSend = async ({isProduction=1, sysName, sourceHeaders, userIdentifier, action, targetType, time=new Date(), targetID,  targetOwnerIdentifier, extraInfo, _resendTimes=0}) => {
-    if(_debugTrace){
-        console.log('vlogSend...')
+/**
+ * 发送日志
+ * @param isProduction  是否为生产环境
+ * @param sysName       业务方系统简称（二级域名的第一个单词缩写）
+ * @param sourceHeaders 源请求req头
+ * @param userIdentifier    用户标识
+ * @param action            动作名称
+ * @param targetType        操作目标
+ * @param [time]            可选，动作事件的发生时间，默认为当前时间
+ * @param targetID          操作目标的标识符
+ * @param targetOwnerIdentifier 操作目标归属方的标识符
+ * @param extraInfo         附加信息对象
+ * @returns {Promise<void>}
+ */
+const vlogSend = async ({isProduction=1, sysName, sourceHeaders, userIdentifier, action, targetType, time=new Date(), targetID,  targetOwnerIdentifier, extraInfo}) => {
+    if (!_apiUrl) {
+        throw new Error(`TransferVLog中的apiUrl尚未配置，请先调用vlog_setting设置`)
     }
-    // if (!_apiUrl) {
-    //     throw new Error(`TransferVLog中的apiUrl尚未配置，请调用vlog_setting设置`)
-    // }
     // if (_apiUrl.indexOf("gankao.com") !== -1) {
     //     throw new Error(`TransferVLog中的apiUrl请配置为云服务的IP，以减少DNS解析开销`)
     // }
-    if (!sysName) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少sysName参数`)
+    if (!sysName || typeof sysName !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少sysName参数 或参数不是字符串类型`)
     }
-    if (!userIdentifier) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少 userIdentifier 参数`)
+    if (!userIdentifier || typeof userIdentifier !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 userIdentifier 参数 或参数不是字符串类型`)
     }
-    if (!action) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少 action 参数`)
+    if (!action || typeof action !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 action 参数 或参数不是字符串类型`)
     }
-    if (!targetType) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少 targetType 参数`)
+    if (!targetType || typeof targetType !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 targetType 参数 或参数不是字符串类型`)
     }
-    if (!targetID) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少 targetID 参数`)
+    if (!targetID || typeof targetID !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 targetID 参数 或参数不是字符串类型`)
     }
-    if (!sourceHeaders) {
-        throw new Error(`TransferVLog的日志发送send调用，缺少 sourceHeaders 参数`)
+    if (!time || typeof time !== "string") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 time 参数 或参数不是字符串类型`)
+    }
+    if (!sourceHeaders || typeof sourceHeaders !== "object") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 sourceHeaders 参数 或参数不是object类型`)
+    }
+    if (!extraInfo || typeof extraInfo !== "object") {
+        throw new Error(`TransferVLog的日志发送send调用，缺少 sourceHeaders 参数 或参数不是object类型`)
+    }
+    if (_debugTrace) {
+        console.log('vlogSend... ')
     }
     workerProcess.send({
         _debugTrace,
-        _uuid:uuidv4(),
+        _uuid: uuidv4(),
         _apiUrl,
-        isProduction,
+        isProduction: (isProduction === 1 || isProduction === '1' || isProduction === true) ? 1 : 0,
         sysName,
         userIdentifier,
         action,
@@ -63,7 +83,7 @@ const vlogSend = async ({isProduction=1, sysName, sourceHeaders, userIdentifier,
         targetOwnerIdentifier,
         extraInfo,
         sourceHeaders,
-        _resendTimes
+        _resendTimes: 0
     });
 }
 
--- test/vlog.js
+++ test/vlog.js
@@ -1,4 +1,5 @@
 import {vlog_setting, vlogSend} from '../src/lib/vloghelper/index.js'
+import moment from 'moment';
 
 vlog_setting({apiUrl:"http://vlog.api.gankao.com/gktag/savevLog", debugTrace:true});
 
@@ -8,13 +9,13 @@ const TestSend =()=> {
         vlogSend({
             isProduction: 1,
             sysName: 'dashi',
-            sourceHeaders: {a: '1'},
-            userIdentifier: Math.floor(Math.random() * 100000),
+            userIdentifier: Math.floor(Math.random() * 100000).toString(),
             action: '关注',
             targetType: '公众号',
-            time: new Date(),
-            targetID: Math.floor(Math.random() * 100000),
-            targetOwnerIdentifier: Math.floor(Math.random() * 100000),
+            time: moment().format('YYYY-MM-DD HH:MM:ss'),
+            targetID: Math.floor(Math.random() * 100000).toString(),
+            targetOwnerIdentifier: Math.floor(Math.random() * 100000).toString(),
+            sourceHeaders: {a: '1'},
             extraInfo: {}
         }).then(() => {
             tCount++;
